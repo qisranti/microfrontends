057abb60175d7984ca804227b18117f779cb50de
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2025-08-26 15:34:39 -0400

    Implemented toSignal instead of subscriptions for remote2

--- data-resolvers-workspace/projects/remote2/src/app/app.ts
+++ data-resolvers-workspace/projects/remote2/src/app/app.ts
@@ -1,19 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-remote2-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('remote2');
-  name = '';
+  readonly name = signal('');
 
-  constructor(private router: ActivatedRoute){
-    this.router.params.subscribe(params => {
-      this.name = params['name'] || '';
-    });
+  constructor(private router: ActivatedRoute) {
+    const nameSignal = toSignal(
+      this.router.params.pipe(map(params => params['name'] || ''))
+    );
+
+    this.name.set(nameSignal());
   }
+
 }
