503f152613df7413550d8f073d67b05004d76e50
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2025-09-09 18:52:07 -0400

    styling for the battlefield

--- pokedex-mfe/projects/shell/src/app/features/battle/battle.component.ts
+++ pokedex-mfe/projects/shell/src/app/features/battle/battle.component.ts
@@ -1,26 +1,29 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PokemonService } from 'pokelib';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-battle',
   imports: [MatProgressBarModule],
   templateUrl: './battle.component.html',
-  styleUrls: ['./battle.component.scss']
+  styleUrls: ['./battle.component.scss'],
 })
-export class BattleComponent{
+export class BattleComponent {
   readonly #pokemonService = inject(PokemonService);
-  
-  constructor() { 
-    this.#pokemonService.getPokemonStats(1).subscribe(stats => {
+
+  player1Pokemons = [
+    { name: 'Bulbasaur', id: 1 },
+    { name: 'Charmander', id: 4 },
+    { name: 'Squirtle', id: 7 },
+  ];
+
+  constructor() {
+    this.#pokemonService.getPokemonStats(1).subscribe((stats) => {
       console.log('Bulbasaur stats:', stats);
     });
 
-    this.#pokemonService.getPokemonStats(2).subscribe(stats => {
+    this.#pokemonService.getPokemonStats(2).subscribe((stats) => {
       console.log('Ivysaur stats:', stats);
-    }); 
+    });
   }
-
-
 }
