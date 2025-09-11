1ba88645de1a042711c86dc4f37e1474e01ff6f1
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2025-09-11 17:21:48 +0200

    Add TODO comments in PokemonIdPipe
    Added TODO comments for future improvements.

--- pokelib/projects/pokelib/src/lib/pipes/pokemonId.pipe.ts
+++ pokelib/projects/pokelib/src/lib/pipes/pokemonId.pipe.ts
@@ -4,11 +4,13 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
   name: 'pokemonId',
   standalone: true
 })
+// @TODO: Remove this decorator
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonIdPipe implements PipeTransform {
 
+  // @TODO: Return a number (make sure to check the type)
   transform(url: string): string {
     const parts = url.split('/');
     return parts[parts.length - 2];
